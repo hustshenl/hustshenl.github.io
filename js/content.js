1066fc433a5966737c42f12cb34cfc0e7492dffd
Shen.L <shen@shenl.com>
2016-08-29 09:18:46 +0800

    更新TOC

--- js/content.js
+++ js/content.js
@@ -1,12 +1,12 @@
 /* jshint asi:true */
 /////////////////////////content////////////////////////////
-generateContent()
+generateContent();
 
 /**
  * [generateContent description]
  */
 function generateContent() {
-    var contentToc = document.querySelector('#markdown-toc')
+    var contentToc = document.querySelector('#markdown-toc');
 
 
     if (contentToc === null) {
@@ -14,14 +14,14 @@ function generateContent() {
     } else {
         //为了平滑滚动，使用了第三方库scroll.js
         //要给每个a标签添加class=scroll
-        var aTags = contentToc.querySelectorAll('a')
+        var aTags = contentToc.querySelectorAll('a');
         for (var i = 0; i < aTags.length; i++) {
             aTags[i].setAttribute('class', 'scroll')
         }
 
         // 将内容转移
-        var contentHtml = contentToc.innerHTML
-        var sideContentUl = document.querySelector('#content-side')
+        var contentHtml = contentToc.innerHTML;
+        var sideContentUl = document.querySelector('#content-side');
         sideContentUl.innerHTML = contentHtml
     }
 
@@ -32,7 +32,7 @@ function generateContent() {
     }
 }
 
-fixSidebar()
+fixSidebar();
 
 /**
  * [fixSidebar description]
@@ -40,20 +40,20 @@ fixSidebar()
  * 反之，取消样式
  */
 function fixSidebar() {
-    var sidebarWrap = document.querySelector('.sidebar-wrap')
+    var sidebarWrap = document.querySelector('.sidebar-wrap');
     window.onscroll = function() {
-        var sidebarWrapTop = sidebarWrap.getBoundingClientRect().top
+        var sidebarWrapTop = sidebarWrap.getBoundingClientRect().top;
         if (sidebarWrapTop < 21) {
             sidebarWrap.classList.add('fixed')
         }
-        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
         if (scrollTop < 78) {
             sidebarWrap.classList.remove('fixed')
         }
     }
 }
 
-controlHeight()
+controlHeight();
 
 /**
  * [controlHeight description]
@@ -63,11 +63,11 @@ function controlHeight() {
     //先获取similar posts 的高度
     //和用户浏览器窗口的高度
     //给content设置最大高度
-    var similarDiv = document.querySelector('.related-post')
-    var contentUl = document.querySelector('.content-ul')
-    var similarDivHeight = similarDiv.offsetHeight
-    var windowHeight = window.innerHeight
-    var contentMaxHeight = windowHeight - similarDivHeight - 77 - 60
+    var similarDiv = document.querySelector('.related-post');
+    var contentUl = document.querySelector('.content-ul');
+    var similarDivHeight = similarDiv.offsetHeight;
+    var windowHeight = window.innerHeight;
+    var contentMaxHeight = windowHeight - similarDivHeight - 77 - 60;
 
     contentUl.style.maxHeight = contentMaxHeight + 'px'
 }
